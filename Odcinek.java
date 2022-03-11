87231bdf1223471536deb16e686e3a97dca69e07
Wiktorpluzek <75083416+Wiktorpluzek@users.noreply.github.com>
2022-03-11 14:59:10 +0100

    Add files via upload

--- Odcinek.java
+++ Odcinek.java
@@ -5,4 +5,9 @@ public class Odcinek {
     public Odcinek(){
         this(new Punkt(0,0), new Punkt(0,0));
     }
+
+    public Odcinek(Punkt p1, Punkt p2){
+        this.p1 = p1;
+        this.p2 = p2;
+    }
 }
